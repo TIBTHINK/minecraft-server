6236074c04d2374c8b5be0a7507d318836ef44fb
TIBTHINK <38056175+TIBTHINK@users.noreply.github.com>
2021-03-08 05:13:52 -0500

    added in windows support

--- init-server.py
+++ init-server.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
 import os
 from time import sleep
 from os import system as cmd
-from subprocess import run
+import subprocess
 import platform
 import requests
 import json
 import multiprocessing
-
+import argparse 
 
 response = requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
 output = response.json()
@@ -17,12 +16,26 @@ core_count = str(multiprocessing.cpu_count())
 punctuation = '''"'''
 pwd = os.getcwd()
 user = os.getlogin()
+response = requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
+output = response.json()
+data = json.dumps(output['latest']['release'])
+remove_punct = ""
+for character in data:
+    if character not in punctuation:
+        remove_punct = remove_punct + character
+latest_release = remove_punct
+core_count = str(multiprocessing.cpu_count())
+system = platform.system()
+if system == "Windows":
+    type_of_os = "bat"
+else:
+    type_of_os = "sh"
+
 def update_server():
-    open("./update-server.sh", "w+").write("""
+    open("./update-server." + type_of_os + "", "w+").write("""
     java -jar BuildTools.jar --rev latest
-    rm -rf ./plugins/Geyser-Spigot.jar
-    wget -P ./plugins/ https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar 
 """)
+
 def make_main_world():
     open("./makeMainWorld.py", "w+").write("""from os import system as cmd
     import os
@@ -62,43 +75,35 @@ WantedBy=multi-user.target
 
 
 try:
-    system = platform.system()
-    if system == "Windows":
-        exit("This script was Built for Linux, Windows support will be added in the future")
-       
-    response = requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
-    output = response.json()
-    data = json.dumps(output['latest']['release'])
-    string = data
-    remove_punct = ""
-    for character in data:
-        if character not in punctuation:
-            remove_punct = remove_punct + character
-    latest_release = remove_punct
-
-    core_count = str(multiprocessing.cpu_count())
     version = input("What version of minecraft do you want? (Defult is the lastest version): ") or latest_release
-    ram = input("how much ram would you like the server to use (Defult is 2GB): ") or "2"
-    cpu_cores = input("how many cores does your cpu have (Defult is how many cores you have): ") or core_count
+    ram = input("how much ram would you like the server to use (Defult is 2048MB): ") or "2048"
+    cpu_cores = input("how many cores do you want to give to the server (Defult is how many cores you have): ") or core_count
+    build_env = "java -jar BuildTools.jar --rev " + version 
     print("Checking if BuildTools in installed")
     if not os.path.isfile("BuildTools.jar"):
         print("Downloading required files")
-        myfile = requests.get("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar")
-        open(pwd + "/BuildTools.jar", 'wb').write(myfile.content)
+        open(pwd + "/BuildTools.jar", 'wb').write(requests.get("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar").content)
 
     open("./eula.txt", "w+").write("eula=true")
-    open("./start.sh", "w+").write("java -server -XX:+UseConcMarkSweepGC -XX:ParallelGCThreads=" + cpu_cores + " -XX:+AggressiveOpts -Xms256M -Xmx" + ram + "G -jar spigot-" + version + ".jar nogui ")
+    open("./start."+ type_of_os + "", "w+").write("java -server -XX:+UseConcMarkSweepGC -XX:ParallelGCThreads=" + cpu_cores + " -XX:+AggressiveOpts -Xms256M -Xmx" + ram + "M -jar spigot-" + version + ".jar nogui ")
     service_file()
     make_main_world()
     update_server()
-                                                                                                                                        
-    cmd("java -jar BuildTools.jar --rev " + version)
+
+    if type_of_os == "sh":
+         cmd(build_env +"> /dev/null")
+    else:
+        subprocess.call(build_env, stdout = subprocess.PIPE)
+        
+
 
     start_server = input("Would you like to start the server?[y/N]: ") or "n"
     if start_server == "y":
-        cmd("bash start.sh")
+        if type_of_os == "sh":
+            cmd("bash start.sh")
+        else:
+            cmd("start.bat")
     else:
         print("\nCongrats, you have just installed Spigot. I recommend turning on mcrcon for easy terminal access.")
 except KeyboardInterrupt:
     print("\n\nbye") 
- 
