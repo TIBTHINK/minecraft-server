32bdc5a6b688acef0a92d7c780a42c9996dd977f
BuildTools <unconfigured@null.spigotmc.org>
2021-06-26 01:25:57 +0000

    i did stuff

--- init-server.py
+++ init-server.py
@@ -52,28 +52,27 @@ else:
         if not os.path.isdir(path + path2):
             os.mkdir(path, mode)
             os.mkdir(path2, mode)
-        else:  
-            exit
-
+        else:
+            return
+        cmd("git clone https://github.com/Tiiffi/mcrcon.git tools/mcrcon")
+        cmd("cd tools/mcron")
+        cmd("gcc -std=gnu11 -pedantic -Wall -Wextra -O2 -s -o mcrcon mcrcon.c")
 
     def update_server():
-        open("./update-server." + type_of_os + "", "w+").write("""
-java -jar BuildTools.jar --rev """ + version + """
-    """)
+        open("./update-server." + type_of_os + "", "w+").write("""java -jar BuildTools.jar --rev """ + version )
 
     def make_main_world():
         open("./makeMainWorld.py", "w+").write("""from os import system as cmd
 import os
-from os import system as cmd
-                
+from os import system as cmd            
 if os.geteuid() != 0:
     exit("please run me as root")
 else:
-cmd("systemctl stop minecraft.service")
-cmd("cp minecraft.service /etc/systemd/system/minecraft.service")
-cmd("systemctl daemon-reload")
-cmd("systemctl start minecraft.service")
-cmd("systemctl enable minecraft.service")
+    cmd("systemctl stop minecraft.service")
+    cmd("cp minecraft.service /etc/systemd/system/minecraft.service")
+    cmd("systemctl daemon-reload")
+    cmd("systemctl start minecraft.service")
+    cmd("systemctl enable minecraft.service")
             """)
 
     def service_file():
@@ -102,6 +101,7 @@ WantedBy=multi-user.target
         ram = input("How much ram would you like the server to use (Defult is 2048MB): ") or "2048"
         cpu_cores = input("How many cores do you want to give to the server (Defult is how many cores you have): ") or core_count
         port = input("Which port do you want the server to be on (Defult is 25565): ") or "25565"
+        
         if type_of_os == "sh":
             service_file_name = input("Name of the service file to be generated? (Defult is minecraft): ") or "minecraft"
         build_env = "java -jar BuildTools.jar --rev " + version 
@@ -111,14 +111,17 @@ WantedBy=multi-user.target
             open(pwd + "/BuildTools.jar", 'wb').write(requests.get("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar").content)
 
         open("./eula.txt", "w+").write("eula=true")
-        open("./start."+ type_of_os + "", "w+").write("java -server -XX:+UseConcMarkSweepGC -XX:ParallelGCThreads=" + cpu_cores + " -XX:+AggressiveOpts -Xms256M -Xmx" + ram + "M -jar spigot-" + version + ".jar nogui ")
+        open("./start."+ type_of_os + "", "w+").write("java -server -XX:ParallelGCThreads=" + cpu_cores + " -Xms256M -Xmx" + ram + "M -jar spigot-" + version + ".jar nogui ")
         open("server.properties", "w+").write("server-port=" + port + "")
         if type_of_os == "sh":
             service_file()
             make_main_world()
             update_server()
+            print("seting up mcron")
+            mcron_setup()
         else:
             update_server()
+        
         cmd(build_env)      
         start_server = input("Would you like to start the server?[y/N]: ") or "n"
         if start_server == "y":
