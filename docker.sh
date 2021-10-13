cc76ab4d9f02602f38b389087353a8d3ba940819
tibthink <bent23jam@gmail.com>
2021-10-13 22:18:15 +0000

    sweep sweep sweep

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 docker rmi minecraft-test
 docker buildx build --progress plain -t minecraft-server .
 docker run -td --name minecraft-server minecraft-test
-# docker stop minecraft-server
-# docker rm minecraft-server
+docker stop minecraft-server
+docker rm minecraft-server
