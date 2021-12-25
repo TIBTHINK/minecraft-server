e82c5a8292cfba2f302183db03682906390c159b
tibthink <bent23jam@gmail.com>
2021-12-25 00:23:41 +0000

    fixed it, did a back to the future shit and fixed it

--- init-server.py
+++ init-server.py
@@ -20,6 +20,11 @@ data = json.dumps(output['latest']['release'])
 core_count = multiprocessing.cpu_count()
 SECRET_KEY = os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False)
 
+if SECRET_KEY:
+    user = 'minecraft'
+else:
+    user = 'minecraft'
+    # user = os.getlogin()
 
 system = platform.system()
 if system == "Windows":
@@ -34,100 +39,98 @@ for character in data:
 
 latest_release = remove_punct
 
+try:
+  
+    @click.command()
+    @click.option("-v", "--version", is_flag=False, flag_value=latest_release, default=latest_release, help="Choose what version of the game(Defult: " + latest_release + ")")
+    @click.option("-c", "--cores", default=core_count, prompt="How many cores do you want to give to the server: ", help="Set how many cores you want the server to use")
+    @click.option("-r", "--ram", default=2048, prompt="How much ram would you like the server to use", help="Set how much allocated ram to the server")
+    @click.option("-p", "--port", default=25565, prompt="Which port do you want the server to be on", help="Set what port you want the server to run on")
+    @click.option("-s", "--service", is_flag=False, flag_value="minecraft", default="minecraft", help="Sets the service name(Optional)")
+    @click.option("-P", "--pluginpack", is_flag=True, flag_value=True, help="Generates a script of essential spigot plugins(Optional)")
 
-@click.command()
-@click.option("-v", "--version", is_flag=False, flag_value=latest_release, default=latest_release, help="Choose what version of the game(Defult: " + latest_release + ")")
-@click.option("-c", "--cores", default=core_count, prompt="How many cores do you want to give to the server: ", help="Set how many cores you want the server to use")
-@click.option("-r", "--ram", default=2048, prompt="How much ram would you like the server to use", help="Set how much allocated ram to the server")
-@click.option("-p", "--port", default=25565, prompt="Which port do you want the server to be on", help="Set what port you want the server to run on")
-@click.option("-s", "--service", is_flag=False, flag_value="minecraft", default="minecraft", help="Sets the service name(Optional)")
-@click.option("-P", "--pluginpack", is_flag=True, flag_value=True, help="Generates a script of essential spigot plugins(Optional)")
-@click.option("-d", "--debug", is_flag=True, flag_value=True, help="Allows to run a debug on any machine with defult vaules")
-@click.option("-y", "--yes", is_flag=True, flag_value=True, help="You can have the server start on its own ")
-def main(version, cores, ram, port, service, pluginpack, debug, yes):
 
-    version = version
-    cores = cores
-    ram = ram
-    port = port
-    service = service
-    pluginpack = pluginpack
-    debug = debug
-    yes = yes
+    def main(version, cores, ram, port, service, pluginpack):
 
-    if SECRET_KEY or debug:
-        user = 'minecraft'
-    else:
-        user = os.getlogin()
+        def plugin_pack_script_gen():
+            open("./pluginpack.py", "w+").write("""# Yes i know, i could find a way to get the name of the jar file,
+# but i am not motivated to give a fuck about it so you guys just
+# have to deal with the lazyness 
 
-    if pluginpack:
-        open("./pluginpack.py", "w+").write("""import os
+import os
 import json
 import requests
-
 pwd = str(os.getcwd())
 class plugins():
     def github_downloader(url, name, sub=1):
         folder_check = os.path.exists(pwd + "/plugins") 
-        if folder_check:
-            print("Folder already exist")
-        else:
+        if not folder_check:
             path = os.path.join(pwd, "plugins")
             os.mkdir(path)
-        dynmap_response = requests.get(url)
-        data = dynmap_response.json()
+        response = requests.get(url)
+        data = response.json()
         spigot_number = len(data[1]['assets'])
         download_link = data[1]['assets'][spigot_number - sub]['browser_download_url']
         open(pwd + "/plugins/" + name, 'wb').write(requests.get(download_link).content)
     def github_downloader_sr(url, name,):
         folder_check = os.path.exists(pwd + "/plugins") 
-        if folder_check:
-            print("Folder already exist")
-        else:
+        if not folder_check:
             path = os.path.join(pwd, "plugins")
-            os.mkdir(path)
-        dynmap_response = requests.get(url)
-        data = dynmap_response.json()
+            os.mkdir(path)         
+        response = requests.get(url)
+        data = response.json()
         download_link = data[0]['assets'][0]['browser_download_url']
         open(pwd + "/plugins/" + name, 'wb').write(requests.get(download_link).content)
-
 if __name__ == '__main__':
     plugins.github_downloader("https://api.github.com/repos/webbukkit/dynmap/releases", "Dynmap.jar")
     plugins.github_downloader("https://api.github.com/repos/PryPurity/WorldBorder/releases", "WorldBorder.jar")
     plugins.github_downloader("https://api.github.com/repos/EssentialsX/Essentials/releases", "EssentialsX.jar", 8 )
     plugins.github_downloader_sr("https://api.github.com/repos/TIBTHINK/payRespect/releases", "PayRespect.jar")""")
-        from pluginpack import plugins
-        plugins.github_downloader("https://api.github.com/repos/webbukkit/dynmap/releases", "Dynmap.jar")
-        plugins.github_downloader("https://api.github.com/repos/PryPurity/WorldBorder/releases", "WorldBorder.jar")
-        plugins.github_downloader("https://api.github.com/repos/EssentialsX/Essentials/releases", "EssentialsX.jar", 8)
-        plugins.github_downloader_sr("https://api.github.com/repos/TIBTHINK/payRespect/releases", "PayRespect.jar")
+            from pluginpack import plugins
+            plugins.github_downloader("https://api.github.com/repos/webbukkit/dynmap/releases", "Dynmap.jar")
+            plugins.github_downloader("https://api.github.com/repos/PryPurity/WorldBorder/releases", "WorldBorder.jar")
+            plugins.github_downloader("https://api.github.com/repos/EssentialsX/Essentials/releases", "EssentialsX.jar", 8)
+            plugins.github_downloader_sr("https://api.github.com/repos/TIBTHINK/payRespect/releases", "PayRespect.jar")
 
-    def service_file(service):
-        open("/" + service + ".service", "w+").write("""[Unit]
-    Description=Minecraft Server
-    After=network.target
-    [Service]
-    User=""" + user + """
-    Nice=1
-    KillMode=none
-    SuccessExitStatus=0 1
-    ProtectHome=true
-    ProtectSystem=full
-    PrivateDevices=true
-    NoNewPrivileges=true
-    WorkingDirectory=""" + pwd + """
-    ExecStart= /usr/bin/bash """ + pwd + """/start.sh
-    ExecStop=/usr/bin/mcrcon -H 127.0.0.1 -P 25575 -p password stop
-    ExecReload=/usr/bin/mcrcon -H 127.0.0.1 -P 25575 -p password restart
-    [Install]
-    WantedBy=multi-user.target""")
 
-    def update_server(type_of_os, version):
-        open("./update-server." + type_of_os + "", "w+").write("""java -jar BuildTools.jar --rev """ + version)
+        def service_file():
+            open("./" + service + ".service", "w+").write("""[Unit]
+Description=Minecraft Server
+After=network.target
+[Service]
+User=""" + user + """
+Nice=1
+KillMode=none
+SuccessExitStatus=0 1
+ProtectHome=true
+ProtectSystem=full
+PrivateDevices=true
+NoNewPrivileges=true
+WorkingDirectory=""" + pwd + """
+ExecStart= /usr/bin/bash """ + pwd +"""/start.sh
+ExecStop=/usr/bin/mcrcon -H 127.0.0.1 -P 25575 -p password stop
+ExecReload=/usr/bin/mcrcon -H 127.0.0.1 -P 25575 -p password restart
+[Install]
+WantedBy=multi-user.target
+            """)
+        # def mcron_setup():
 
-    def make_main_world():
-        open("./makeMainWorld.py", "w+").write("""from os import system as cmd
+        #     cmd("git clone https://github.com/Tiiffi/mcrcon.git mcrcon")
+        #     cmd("cd mcron")
+        #     cmd("make")
+        #     cmd("sudo make install")
+        #     cmd("cd " + pwd)
+
+
+        def update_server():
+            open("./update-server." + type_of_os + "",
+                "w+").write("""java -jar BuildTools.jar --rev """ + version)
+
+
+        def make_main_world():
+            open("./makeMainWorld.py", "w+").write("""from os import system as cmd
 import os
+from os import system as cmd
 if os.geteuid() != 0:
     exit("please run me as root")
 else:
@@ -135,37 +138,35 @@ else:
     cmd("cp minecraft.service /etc/systemd/system/minecraft.service")
     cmd("systemctl daemon-reload") 
     cmd("systemctl start minecraft.service")
-    cmd("systemctl enable minecraft.service")""")
+    cmd("systemctl enable minecraft.service")
+                """)
 
-
-    def backend(pwd, cores, ram, version, port):
         print("Checking if BuildTools in installed")
         if not os.path.isfile("BuildTools.jar"):
             print("###DOWNLOADING REQUIRED FILES###")
-            open(pwd + "BuildTools.jar", 'wb+').write(requests.get("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar").content)
+            open(pwd + "/BuildTools.jar", 'wb').write(requests.get("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar").content)
         # Auto accpeting eula, making a sick start script and setting custom ports
         open("eula.txt", "w+").write("eula=true")
-        open("start." + type_of_os, "w+").write("java -server -XX:ParallelGCThreads=" + str(cores) +" -Xms256M -Xmx" + str(ram) + "M -jar " + pwd + "/spigot-" + version + ".jar nogui")
+        open("start." + type_of_os + "", "w+").write("java -server -XX:ParallelGCThreads=" + str(cores) + " -Xms256M -Xmx" + str(ram) + "M -jar " + pwd +  "/spigot-" + version + ".jar nogui ")
         open("server.properties", "w+").write("server-port=" + str(port) + "")
         # checking if this server supports custom scripts
         if type_of_os == "sh":
             service_file()
             make_main_world()
-            update_server()
+            update_server()                    
         else:
             update_server()
         # Listen, we dont like no docker containers
         # Checks to understand what to do
-        if SECRET_KEY or yes:
-            start_server = "y"
-            cmd("java -jar BuildTools.jar --rev " + version)
-        elif debug:
+        if SECRET_KEY:
+            print("SWEET CAROLINE")
             start_server = "n"
-            cmd("java -jar BuildTools.jar --rev " + version)
         else:
             cmd("java -jar BuildTools.jar --rev " + version)
-            start_server = input(
-                "Would you like to start the server? [y/N]") or "n"
+            start_server = input("Would you like to start the server? [y/N]") or "n"
+
+        if pluginpack:
+            plugin_pack_script_gen()
         # Auto start server
         if start_server == "y":
             if type_of_os == "sh":
@@ -176,6 +177,9 @@ else:
             print("You can start the server with ./start." + type_of_os)
 
 
-if __name__ == '__main__':
-    main()
-    main.backend()
+    if __name__ == '__main__':
+        main()
+    
+
+except KeyboardInterrupt:
+    print("\nbye")
--- pluginpack.py
+++ pluginpack.py
@@ -1,33 +1,31 @@
+# Yes i know, i could find a way to get the name of the jar file,
+# but i am not motivated to give a fuck about it so you guys just
+# have to deal with the lazyness 
+
 import os
 import json
 import requests
-
 pwd = str(os.getcwd())
 class plugins():
     def github_downloader(url, name, sub=1):
         folder_check = os.path.exists(pwd + "/plugins") 
-        if folder_check:
-            print("Folder already exist")
-        else:
+        if not folder_check:
             path = os.path.join(pwd, "plugins")
             os.mkdir(path)
-        dynmap_response = requests.get(url)
-        data = dynmap_response.json()
+        response = requests.get(url)
+        data = response.json()
         spigot_number = len(data[1]['assets'])
         download_link = data[1]['assets'][spigot_number - sub]['browser_download_url']
         open(pwd + "/plugins/" + name, 'wb').write(requests.get(download_link).content)
     def github_downloader_sr(url, name,):
         folder_check = os.path.exists(pwd + "/plugins") 
-        if folder_check:
-            print("Folder already exist")
-        else:
+        if not folder_check:
             path = os.path.join(pwd, "plugins")
-            os.mkdir(path)
-        dynmap_response = requests.get(url)
-        data = dynmap_response.json()
+            os.mkdir(path)         
+        response = requests.get(url)
+        data = response.json()
         download_link = data[0]['assets'][0]['browser_download_url']
         open(pwd + "/plugins/" + name, 'wb').write(requests.get(download_link).content)
-
 if __name__ == '__main__':
     plugins.github_downloader("https://api.github.com/repos/webbukkit/dynmap/releases", "Dynmap.jar")
     plugins.github_downloader("https://api.github.com/repos/PryPurity/WorldBorder/releases", "WorldBorder.jar")
